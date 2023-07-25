ab20474457c7ed1063ad274beda92ffa4d63ca7b
Dillon Townsend <dtownsend@contentactive.com>
2023-07-25 07:34:00 -0500

    added script

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# React + Vite
+# how to run script
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+1. checkout to the branch you want to build
+2. ensure bash script has execution permission `chmod +x update_and_build.sh`
+3. run the script `./update_and_build.sh`
---
+++ update_and_build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Step 1: Check for changes in the remote repository
+git remote update
+
+# Get the name of the current branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Check if the current branch is tracking a remote branch
+if [ -n "$(git ls-remote --exit-code origin "$current_branch")" ]; then
+  # Compare the local and remote branches to check for any differences
+  git fetch
+  if git diff HEAD..origin/"$current_branch" --exit-code; then
+    echo "No changes in the remote repository."
+  else
+    # Step 2: If there are changes, pull them from the remote repository
+    git pull origin "$current_branch"
+  fi
+else
+  echo "The current branch is not tracking a remote branch. Please configure tracking."
+  exit 1
+fi
+
+# Step 3: Build the React app
+npm run build
