72ca6f9bd9be44a6fd980e937ba0f2c065caef8e
dtownsendca <140512026+dtownsendca@users.noreply.github.com>
2023-07-26 07:42:27 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more. Test from dillon 4.
+        Click on the Vite and React logos to learn more. Test from dillon 5.
       </p>
     </>
   )
